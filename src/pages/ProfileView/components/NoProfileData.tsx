02a8e8d16f88c03452d4bef9a119cf3249a120d1
Michał Roman <michalr84@gmail.com>
2025-06-08 13:59:03 +0200

    fixed a few paddings in the layout

--- src/pages/ProfileView/components/NoProfileData.tsx
+++ src/pages/ProfileView/components/NoProfileData.tsx
@@ -5,16 +5,10 @@ import { Button } from "../../../components/carbon-ui";
 export const NoProfileData = () => (
   <Card className="card--max-width-2xl card--borderless">
     <CardContent className="no-profile">
-      <div className="no-profile__icon">
-        <p className="no-profile__title">No Profile Data Found</p>
-      </div>
       <p className="no-profile__description">No profile data found.</p>
       <div className="no-profile__action">
         <Link to="/">
-          <Button
-            type="submit"
-            className="btn btn--primary"
-          >
+          <Button type="submit" className="btn btn--primary">
             <span>Create Profile</span>
           </Button>
         </Link>
