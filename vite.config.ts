1f523cdb936d2eaee3d021bfb17b6a90b66d50d8
Michał Roman <michalr84@gmail.com>
2025-06-09 10:47:51 +0200

    silenced carbon related deprecation warnings in the terminal. Will handle it later

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true,
+        silenceDeprecations: ['mixed-decls', 'global-builtin']
+      }
+    }
+  },
 })
